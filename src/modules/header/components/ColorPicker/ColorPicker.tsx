914fa2ec14f4eab97d082304f24d845ddd0b0544
do0dleman <97869897+do0dleman@users.noreply.github.com>
2023-11-15 19:49:34 +0200

    reworked hsl colorpicker

--- src/modules/header/components/ColorPicker/ColorPicker.tsx
+++ src/modules/header/components/ColorPicker/ColorPicker.tsx
@@ -1,71 +1,67 @@
-import React, { useEffect, useRef, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import './ColorPicker.scss'
-import { Color } from 'do0dle-colors'
+import { Color, ConversionFunctions } from 'do0dle-colors'
 import { useSeedColor } from '../../../../hooks/useSeedColor'
 import { useSetSeedColor } from '../../../../hooks/useSetSeedColor'
 import _ from 'lodash'
 
-interface ColorPickerProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLDivElement> {
-}
+interface ColorPickerProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLDivElement> { }
 export default function ColorPicker(props: ColorPickerProps) {
 
     const { className, ...rest } = props
 
     const seedColor = useSeedColor()
+    const seedHsl = seedColor.getHslArray()
     const setSeedColor = useSetSeedColor()
 
-    const hueRef = useRef<HTMLInputElement>(null)
-    const satRef = useRef<HTMLInputElement>(null)
-    const lightRef = useRef<HTMLInputElement>(null)
-
     const [hex, setHex] = useState<string>('d')
-    const [localSeed, setLocalSeed] = useState(new Color(seedColor))
+
+    const [hue, setHue] = useState(seedHsl[0])
+    const [saturation, setSaturation] = useState(seedHsl[1])
+    const [lightness, setLightness] = useState(seedHsl[2])
 
     useEffect(() => {
-        const hsl = seedColor.getHslArray()
-        hueRef.current!.value = `${Math.round(hsl[0])}`
-        satRef.current!.value = `${Math.round(hsl[1])}`
-        lightRef.current!.value = `${Math.round(hsl[2])}`
-        if (!_.isEqual(seedColor, localSeed)) setLocalSeed(seedColor)
-        // setHex(seedColor.getCssHex().toUpperCase())
-    }, [seedColor])
-
-    useEffect(() => {
-        setHex(localSeed.getCssHex())
-    }, [localSeed])
+        const { hslToRgb, rgbToHex } = ConversionFunctions
+        setHex(rgbToHex(hslToRgb([hue, saturation / 100, lightness / 100])))
+    }, [hue, saturation, lightness])
 
 
     function HandleHueChange(e: React.ChangeEvent<HTMLInputElement>) {
-        const newColor = seedColor.getHslArray()
-        newColor[0] = +e.target.value
-        setSeedColor(new Color(newColor, 'hsl').getOkLChArray())
+        setHue(+e.target.value)
+        setSeedColor(new Color([+e.target.value, saturation, lightness], 'hsl').getOkLChArray())
     }
     function HandleSatChange(e: React.ChangeEvent<HTMLInputElement>) {
-        const newColor = seedColor.getHslArray()
-        newColor[1] = +e.target.value
-        setSeedColor(new Color(newColor, 'hsl').getOkLChArray())
+        setSaturation(+e.target.value)
+        setSeedColor(new Color([hue, +e.target.value, lightness], 'hsl').getOkLChArray())
     }
     function HandleLightChange(e: React.ChangeEvent<HTMLInputElement>) {
-        const newColor = seedColor.getHslArray()
-        newColor[2] = +e.target.value
-        setSeedColor(new Color(newColor, 'hsl').getOkLChArray())
+        setLightness(+e.target.value)
+        setSeedColor(new Color([hue, saturation, +e.target.value], 'hsl').getOkLChArray())
     }
     function HandleHexInputChange(e: React.ChangeEvent<HTMLInputElement>) {
         setHex(e.target!.value)
         let input = e.target!.value
         if (input[0] != '#') input = '#' + input
         setHex(input)
-        const newHsl = new Color(input).getOkLChArray()
-        if (!isNaN(newHsl[0]) && !isNaN(newHsl[1]) && !isNaN(newHsl[2]) && input.length === 7)
-            setSeedColor(newHsl)
+
+        const newColor = new Color(input)
+        const newHsl = newColor.getHslArray()
+        const newOkLCh = newColor.getOkLChArray()
+        if (!isNaN(newOkLCh[0]) && !isNaN(newOkLCh[1]) && !isNaN(newOkLCh[2]) && input.length === 7) {
+            setHue(newHsl[0])
+            setSaturation(newHsl[1])
+            setLightness(newHsl[2])
+
+            setSeedColor(newOkLCh)
+        }
     }
     return (
         <div className='-color-picker' {...rest}>
             <div className="sliders">
                 <div className="inputs">
-                    <input onChange={HandleHueChange} className='range' type="range" ref={hueRef} min={0} max={360} />
-                    <input onChange={HandleSatChange} className='range' type="range" ref={satRef} min={0} max={100} />
-                    <input onChange={HandleLightChange} className='range' type="range" ref={lightRef} min={0} max={100} />
+                    <input onChange={HandleHueChange} value={hue} className='range' type="range" min={0} max={360} />
+                    <input onChange={HandleSatChange} value={saturation} className='range' type="range" min={0} max={100} />
+                    <input onChange={HandleLightChange} value={lightness} className='range' type="range" min={0} max={100} />
                 </div>
                 <div className="color-preview"></div>
             </div>
