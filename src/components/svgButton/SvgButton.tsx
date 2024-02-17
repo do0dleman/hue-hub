b53cb6545e327d5530bf014588e0c9b986e01ea4
do0dleman <97869897+do0dleman@users.noreply.github.com>
2024-02-17 08:25:22 +0200

    added note in SVG button component

--- src/components/svgButton/SvgButton.tsx
+++ src/components/svgButton/SvgButton.tsx
@@ -3,6 +3,7 @@ import './SvgButton.scss'
 interface SvgButtonProps extends React.DetailedHTMLProps<React.HTMLAttributes<HTMLElement>, HTMLButtonElement> {
     className?: string
 }
+//! SVG has to have viewbox for styles to work
 export default function SvgButton(props: SvgButtonProps) {
 
     const { className, children, ...rest } = props
